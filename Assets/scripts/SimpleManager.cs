958685e04b190aa936e53563952c020defc80897
codingInSpace <jonathan.grangien@gmail.com>
2016-09-26 16:57:09 +0200

    Implement simple scripts

---
+++ Assets/scripts/SimpleManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleManager : MonoBehaviour {
+    private static SimpleManager _instance;
+
+    public static SimpleManager Instance { get { return _instance; } }
+
+    public Vector2[] monsterSeats;
+    private float amountMonsterCustomers;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+}
---
+++ Assets/scripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+    private double spawnBaseRate = 0.25;
+    private double spawnRate;
+
+    void Update () {
+        // Poll this value every 5 sec
+        spawnRate = spawnBaseRate;
+    }
+}
